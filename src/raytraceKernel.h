30bcd8c599453680d0635dbce30cedfbab69618b
ishaan13 <ishaan3000@gmail.com>
2013-10-14 04:01:14 -0400

    Code push

--- src/raytraceKernel.h
+++ src/raytraceKernel.h
@@ -20,6 +20,13 @@
     #include <cutil_math.h>
 #endif
 
-void cudaRaytraceCore(uchar4* pos, camera* renderCam, int frame, int iterations, material* materials, int numberOfMaterials, geom* geoms, int numberOfGeoms);
+extern glm::vec3* accumulatorImage;
+
+void cudaAllocateAccumulatorImage(camera *renderCam);
+void cudaFreeAccumulatorImage();
+void cudaClearAccumulatorImage(camera *renderCam);
+void cudaRaytraceCore(uchar4* pos, camera* renderCam, int frame, int iterations, material* materials, int numberOfMaterials, geom* geoms, int numberOfGeoms, cameraData liveCamera);
+
+__host__ __device__ glm::vec3 generateRandomNumberFromThread(glm::vec2 resolution, float time, int x, int y);
 
 #endif
--- src/scene.cpp
+++ src/scene.cpp
@@ -162,6 +162,8 @@ int scene::loadCamera(){
 	vector<glm::vec3> positions;
 	vector<glm::vec3> views;
 	vector<glm::vec3> ups;
+	vector<float> apertures;
+	vector<float> focusPlanes;
     while (!line.empty() && fp_in.good()){
 	    
 	    //check frame number
@@ -172,7 +174,7 @@ int scene::loadCamera(){
         }
 	    
 	    //load camera properties
-	    for(int i=0; i<3; i++){
+	    for(int i=0; i<5; i++){
             //glm::vec3 translation; glm::vec3 rotation; glm::vec3 scale;
             utilityCore::safeGetline(fp_in,line);
             tokens = utilityCore::tokenizeString(line);
@@ -183,6 +185,12 @@ int scene::loadCamera(){
             }else if(strcmp(tokens[0].c_str(), "UP")==0){
                 ups.push_back(glm::vec3(atof(tokens[1].c_str()), atof(tokens[2].c_str()), atof(tokens[3].c_str())));
             }
+			else if(strcmp(tokens[0].c_str(), "APERTURE")==0){
+				apertures.push_back(atof(tokens[1].c_str()));
+			}else if(strcmp(tokens[0].c_str(), "FOCUSPLANE")==0){
+				focusPlanes.push_back(atof(tokens[1].c_str()));
+			}
+			
 	    }
 	    
 	    frameCount++;
@@ -194,10 +202,14 @@ int scene::loadCamera(){
 	newCamera.positions = new glm::vec3[frameCount];
 	newCamera.views = new glm::vec3[frameCount];
 	newCamera.ups = new glm::vec3[frameCount];
+	newCamera.apertures = new float[frameCount];
+	newCamera.focusPlanes = new float[frameCount];
 	for(int i=0; i<frameCount; i++){
 		newCamera.positions[i] = positions[i];
 		newCamera.views[i] = views[i];
 		newCamera.ups[i] = ups[i];
+		newCamera.apertures[i] = apertures[i];
+		newCamera.focusPlanes[i] = focusPlanes[i];
 	}
 
 	//calculate fov based on resolution
